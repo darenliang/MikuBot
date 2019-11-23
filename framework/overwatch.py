6dbc389b7d136318f13d10dc4a337851c7eaac21
Daren Liang <darenliang@outlook.com>
2019-11-23 01:08:39 -0500

    Add useragent to overwatch json request

--- framework/overwatch.py
+++ framework/overwatch.py
@@ -1,7 +1,7 @@
 """This framework file contains api bindings for ow-api.com"""
 
 import json
-import urllib.request
+from urllib.request import Request, urlopen
 
 OW_API = 'https://ow-api.com/v1/stats/pc/us/{0}/complete'
 
@@ -9,11 +9,11 @@ OW_API = 'https://ow-api.com/v1/stats/pc/us/{0}/complete'
 def get_overwatch_data(username):
     """Request overwatch data"""
     username = username.replace('#', '-')
-    with urllib.request.urlopen(OW_API.format(username)) as url:
-        data = json.loads(url.read().decode())
-        if 'error' in data:
-            return None, False
-        return data, True
+    req = Request(OW_API.format(username), headers={'User-Agent': 'Mozilla/5.0'})
+    data = json.loads(urlopen(req).read().decode())
+    if 'error' in data:
+        return None, False
+    return data, True
 
 
 def get_seconds(time):
