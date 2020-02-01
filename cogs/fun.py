493da64bc2fdf602ddfb5d7f65f4f49cdde63830
Daren Liang <darenliang@outlook.com>
2020-02-01 16:16:31 -0500

    Update answers to 8ball

--- cogs/fun.py
+++ cogs/fun.py
@@ -95,8 +95,12 @@ class Fun(commands.Cog):
 
         Please note that a question string is necessary although it is not used.
         """
-        choices = ['It is certain.', 'Outlook good.', 'You may rely on it.', 'Ask again later',
-                   'Concentrate and ask again.', 'Reply hazy, try again.', 'My reply is no', 'My sources say no.']
+        choices = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes - definitely.',
+                   'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.',
+                   'Yes.', 'Signs point to yes.', 'Reply hazy, try again.', 'Ask again later.',
+                   'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.',
+                   "Don't count on it.", 'My reply is no.', 'My sources say no.', 'Outlook not so good.',
+                   'Very doubtful.']
         await ctx.send(random.choice(choices))
 
     @commands.command(name='musicquiz', aliases=['mq', 'quiz'])
