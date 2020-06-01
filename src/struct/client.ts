3a1a00b870c29d282821a54781a76fa276bcedc8
Daren Liang <daren@darenliang.com>
2020-06-01 10:58:44 -0400

    Localize dataset

--- src/struct/client.ts
+++ src/struct/client.ts
@@ -7,12 +7,14 @@ import {GifDatabase} from '../utils/gifDatabase';
 import {join} from 'path';
 import config from '../config.json';
 import musicQuizDataset from '../data/musicquiz_dataset.json';
+import catgirlDataset from '../data/catgirl_dataset.json';
 import {TextChannel, VoiceChannel, VoiceConnection} from 'discord.js';
 
 declare module 'discord-akairo' {
     interface AkairoClient {
         config: any;
         musicQuizDataset: any;
+        catgirlDataset: any;
         DDB: DocumentClient;
         prefixDatabase: PrefixDatabase;
         musicQuizDatabase: MusicQuizDatabase;
@@ -51,6 +53,7 @@ export default class Client extends AkairoClient {
         this.DDB = DDB;
         this.config = config;
         this.musicQuizDataset = musicQuizDataset;
+        this.catgirlDataset = catgirlDataset;
         this.prefixDatabase = new PrefixDatabase(this);
         this.musicQuizDatabase = new MusicQuizDatabase(this);
         this.musicQuizSession = new MusicQuizSession();
