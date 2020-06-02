54f1de167427a1a8e586a917478e48c765d66dfb
Daren Liang <daren@darenliang.com>
2020-06-02 08:44:26 -0400

    Localize headpat

--- src/commands/weeb/headpat.ts
+++ src/commands/weeb/headpat.ts
@@ -1,7 +1,6 @@
 import {Command} from 'discord-akairo';
 import {Message, MessageAttachment, User} from 'discord.js';
 import {MBEmbed} from '../../utils/messageGenerator';
-import {nekolife} from '../../utils/nekolife';
 
 export default class HeadpatCommand extends Command {
     constructor() {
@@ -30,21 +29,13 @@ export default class HeadpatCommand extends Command {
     }
 
     async exec(message: Message, {user}: { user: User }) {
-        nekolife('pat')
-            .then(resp => {
-                const ext = resp.data.url.split('.').pop();
-                const embed = new MBEmbed({
-                    title: user ? `${message.author.username} headpats ${user.username}` : 'Here\'s your headpat.'
-                })
-                    .setImage(`attachment://headpat.${ext}`)
-                    .attachFiles(
-                        [new MessageAttachment(resp.data.url,
-                            `headpat.${ext}`)]);
-                return message.channel.send(embed);
-            })
-            .catch(err => {
-                console.log('ERROR', 'headpat', `Network failure on ${err}`);
-                return message.channel.send(':timer: Request timed out for `headpat`.');
-            });
+        const embed = new MBEmbed({
+            title: user ? `${message.author.username} headpats ${user.username}` : 'Here\'s your headpat.'
+        })
+            .setImage(`attachment://headpat.gif`)
+            .attachFiles(
+                [new MessageAttachment(`https://gitlab.com/darenliang/gif/-/raw/master/data/headpats/${Math.floor(Math.random() * 139) + 1}`,
+                    'headpat.gif')]);
+        return message.channel.send(embed);
     }
 }
