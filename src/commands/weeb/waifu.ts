a9f1dc4010d07bd50112d5585e40d7e9b1d2dc89
Daren Liang <daren@darenliang.com>
2020-06-03 13:39:59 -0400

    Localize command

--- src/commands/weeb/waifu.ts
+++ src/commands/weeb/waifu.ts
@@ -19,12 +19,14 @@ export default class WaifuCommand extends Command {
     }
 
     async exec(message: Message) {
+        const selection = Math.floor(Math.random() * Math.floor(100000));
+        console.log('', '', selection);
         const embed = new MBEmbed({
             title: 'Here\'s your waifu.'
         })
             .setImage('attachment://waifu.jpg')
             .attachFiles(
-                [new MessageAttachment(`https://www.thiswaifudoesnotexist.net/example-${Math.floor(Math.random() * Math.floor(10001))}.jpg`,
+                [new MessageAttachment(`https://gitlab.com/darenliang/${(selection < 50000) ? 'w1' : 'w2'}/-/raw/master/data/example-${selection}.jpg`,
                     'waifu.jpg')]);
         return await message.channel.send(embed);
     }
