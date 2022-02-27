4e530b931a27c2d3140c031e48192d38eedc1fed
Daren Liang <daren@darenliang.com>
2022-02-27 07:53:38 -0800

    Revert buggy respawn logic, will fix in a future rewrite and dependency migration

--- src/main.ts
+++ src/main.ts
@@ -12,20 +12,4 @@ manager.on('shardCreate', shard => {
     tracer.console().info([shard.id], 'Launched shard');
 });
 
-function spawn(retry = false) {
-    if (!retry) {
-        manager.spawn().catch(e => {
-            tracer.console().error('[manager]', 'Failed to spawn: ' + e);
-            tracer.console().info('[manager]', 'Retrying to spawn shards');
-            spawn(true);
-        });
-    } else {
-        manager.respawnAll().catch(e => {
-            tracer.console().error('[manager]', 'Failed to respawn: ' + e);
-            tracer.console().info('[manager]', 'Retrying to respawn shards');
-            spawn(true);
-        });
-    }
-}
-
-spawn();
+manager.spawn();
